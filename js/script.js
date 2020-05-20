dfa215ecaa46ca27fd448f40ec3a028e5ef9a926
Giosuè Lentini <giosuelentini94@outlook.com>
2020-05-20 17:23:59 +0200

    sconto

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-var chilometri = prompt('Quanti chilometri vuoi percorrere?');
+var chilometriDaPercorrere = prompt('Quanti chilometri vuoi percorrere?');
 var etaPasseggero = prompt('Quanti anni hai?');
 
+var euroAlKm = 0.21;
+var totalePrezzoViaggio = chilometriDaPercorrere * euroAlKm;
 
+if (etaPasseggero < 18) {
+    var scontoMinori = ( ( totalePrezzoViaggio * 20 ) / 100 );
+  }
+if (etaPasseggero > 65) {
+    var scontoOver = ( ( totalePrezzoViaggio * 40 ) / 100 );
+  }
 
-console.log(chilometri);
+var sconto = (totalePrezzoViaggio - scontoMinori) || (totalePrezzoViaggio - scontoOver);
+
+console.log(chilometriDaPercorrere);
 console.log(etaPasseggero);
+console.log(euroAlKm);
+console.log(totalePrezzoViaggio);
+console.log(scontoMinori);
+console.log(scontoOver);
+console.log(sconto);
