bbf8a78ac3a36b51eea0a981051cfe606ed12e6c
Giosuè Lentini <giosuelentini94@outlook.com>
2020-05-20 18:59:51 +0200

    sconto

--- js/script.js
+++ js/script.js
@@ -4,24 +4,24 @@ var etaPasseggero = prompt('Quanti anni hai?');
 var euroAlKm = 0.21;
 var totalePrezzoViaggio = chilometriDaPercorrere * euroAlKm;
 
+var sconto = 0;
+
 if (etaPasseggero < 18) {
-    var scontoMinori = ( ( totalePrezzoViaggio * 20 ) / 100 )
+    sconto = ( ( totalePrezzoViaggio * 20 ) / 100 )
   }
 else if (etaPasseggero > 65) {
-    var scontoOver = ( ( totalePrezzoViaggio * 40 ) / 100 )
+  sconto = ( ( totalePrezzoViaggio * 40 ) / 100 );
   }
-else if ((etaPasseggero >= 18) || (etaPasseggero <= 65)) {
+else {
     alert('Niente sconto per te :)');
   }
-var sconto = (totalePrezzoViaggio - scontoMinori) || (totalePrezzoViaggio - scontoOver);
+var prezzoScontato = totalePrezzoViaggio - sconto;
 
 console.log(chilometriDaPercorrere);
 console.log(etaPasseggero);
 console.log(euroAlKm);
 console.log(totalePrezzoViaggio);
-console.log(scontoMinori);
-console.log(scontoOver);
 console.log(sconto);
 
 document.getElementById('prezzo').innerHTML = 'Il prezzo è di ' + totalePrezzoViaggio + '€';
-document.getElementById('sconto').innerHTML = 'Il prezzo scontato è di ' + sconto + '€';
+document.getElementById('prezzoscontato').innerHTML = 'Il prezzo scontato è di ' + prezzoScontato + '€';
